22cd4eb71de29af4f123a36ba98b25115d6ad01c
Corey <corgrammer94@gmail.com>
2016-01-27 21:44:00 -0800

    Test Update

---
+++ Lab1_Intellij/src/com/company/StudentView.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Corey on 1/27/16.
+ */
+public class StudentView {
+    public void printStudentDetails(String studentName, String studentRollNo){
+        System.out.println("Student: ");
+        System.out.println("Name: " + studentName);
+        System.out.println("Roll No: " + studentRollNo);
+    }
+}
---
+++ Lab1_NetBeans/src/lab1/StudentController.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/**
+ * Created by Corey on 1/27/16.
+ */
+public class StudentController {
+    private Student model;
+    private StudentView view;
+
+    public StudentController(Student model, StudentView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setStudentName(String name){
+        model.setName(name);
+    }
+
+    public String getStudentName(){
+        return model.getName();
+    }
+
+    public void setStudentRollNo(String rollNo){
+        model.setRollNo(rollNo);
+    }
+
+    public String getStudentRollNo(){
+        return model.getRollNo();
+    }
+
+    public void updateView(){
+        view.printStudentDetails(model.getName(), model.getRollNo());
+    }
+}
+
